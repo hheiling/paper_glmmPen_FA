636b01bd656c55ffe5c9c61df787a86eac566c3a
hheiling <42752591+hheiling@users.noreply.github.com>
2023-04-24 16:41:20 -0400

    Add files via upload

---
+++ Code_Simulations/Poisson_glmmPen_FA_v2.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -t 36:00:00
+#SBATCH --mem=2g
+#SBATCH -n 1
+#SBATCH --output=Pois_FA_v2_%a.out
+#SBATCH --constraint=rhel8
+
+R CMD BATCH ~/Project2/code/Poisson_glmmPen_FA_v2.R ~/Project2/Rout/Poisson_FA_v2_$SLURM_ARRAY_TASK_ID.Rout
