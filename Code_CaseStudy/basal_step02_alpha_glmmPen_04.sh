3f78cdfb31c674020b55649a70c45d4489568e34
hheiling <42752591+hheiling@users.noreply.github.com>
2023-04-24 16:46:14 -0400

    Add files via upload

---
+++ Code_CaseStudy/basal_step02_alpha_glmmPen_04.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -t 72:00:00
+#SBATCH --mem=2g
+#SBATCH -n 1
+#SBATCH --output=alpha_glmmPen_04_%a.out
+#SBATCH --constraint=rhel8
+
+R CMD BATCH ~/Project2/code/basal_step02_fit_alpha_glmmPen_04.R ~/Project2/Rout/alpha_glmmPen_04_$SLURM_ARRAY_TASK_ID.Rout
