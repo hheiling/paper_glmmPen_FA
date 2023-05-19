2e86a48546507c790b172a118e17d2e3a86b98b9
hheiling <42752591+hheiling@users.noreply.github.com>
2023-05-19 17:11:02 -0400

    Delete Code_CaseStudy directory

--- Code_CaseStudy/SubtypingDemo_condensed/R/Get_subtype_col.R
+++
@@ -1,6 +0,0 @@
-Get_subtype_col <- function(callsTmp, schemaTmp) {
-  i <- which(schemaList %in% schemaTmp)
-  colTmp <- subtypeColList[[i]]
-  colSamp <- colTmp[match(callsTmp,subtypeList[[i]])]
-  return(colSamp)
-}
--- Code_CaseStudy/SubtypingDemo_condensed/README_subtype.md
+++
@@ -1,3 +0,0 @@
-# Subtyping Code
-
-The code in this SubtypingDemo_condensed folder is called within the "step01" case study R code to calculate the basal vs classical pancreatic ductal adenocarinoma cancer subtype. 
